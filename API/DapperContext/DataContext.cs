b1cdff992b584bc290e0c8294f9309bf1d9b1ac2
RealAbu0 <105455070+RealAbu0@users.noreply.github.com>
2022-11-14 18:31:45 +0000

    Add files via upload

---
+++ API/DapperContext/DataContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Sources;
+
+namespace DapperContext
+{
+    public class DataContext
+    {
+        public IConfiguration Configuration;
+        private readonly string _connectionString;
+
+        public DataContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            _connectionString = Configuration.GetConnectionString("Default");
+        }
+
+        public IDbConnection GetConnection()
+            => new SqlConnection(_connectionString);
+    }
+}
