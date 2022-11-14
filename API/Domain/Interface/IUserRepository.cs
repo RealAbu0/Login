c093e441846c0a24049fae8e3dedd278486dd2e0
RealAbu0 <105455070+RealAbu0@users.noreply.github.com>
2022-11-14 18:34:11 +0000

    added domain project

---
+++ API/Domain/Interface/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interface
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<UserModel>> GetAllUsers();
+        Task<UserModel> GetOneUserByID(int id);
+        Task<UserModel> InsertUser(UserModel user);
+        Task<UserModel> UpdateUser(UserModel user);
+        Task<UserModel> DeleteUser(int id);
+    }
+}
---
+++ API/Domain/Model/UserModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Model
+{
+    public class UserModel
+    {
+        public int UserID { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public int PhoneNumber { get; set; }
+        public string EmailAddress { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
