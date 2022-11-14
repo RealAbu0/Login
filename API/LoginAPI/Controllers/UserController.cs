11b35b1f61856880b22a4d545b64fc346e3f9dca
RealAbu0 <105455070+RealAbu0@users.noreply.github.com>
2022-11-14 18:58:40 +0000

    added files

---
+++ API/LoginAPI/Controllers/UserController.cs
@@ -0,0 +1,89 @@
+﻿using Domain.Interface;
+using Domain.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoginAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository _userRepo;
+
+        public UserController(IUserRepository userRepo)
+        {
+            _userRepo = userRepo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllUser()
+        {
+            try
+            {
+                var result = await _userRepo.GetAllUsers();
+                return Ok(result);   
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("id")]
+        public async Task<IActionResult> GetOneUser(int id)
+        {
+            try
+            {
+                var result = await _userRepo.GetOneUserByID(id);
+                return Ok(result);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> InsertUser(UserModel user)
+        {
+            try
+            {
+                var result = await _userRepo.InsertUser(user);
+                return Ok(result);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("id")]
+        public async Task<IActionResult> UpdateUser(UserModel user)
+        {
+            try
+            {
+                var result = await _userRepo.UpdateUser(user);
+                return Ok(result);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("id")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            try
+            {
+                var result = await _userRepo.DeleteUser(id);
+                return Ok(result);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
