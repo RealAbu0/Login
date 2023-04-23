53f8abbcb2254a929da8d72fef87fbb6df1f149d
RealAbu0 <105455070+RealAbu0@users.noreply.github.com>
2023-04-23 19:06:07 +0100

    Add files via upload

--- API/LoginAPI/Controllers/UserController.cs
+++ API/LoginAPI/Controllers/UserController.cs
@@ -1,7 +1,17 @@
 ﻿using Domain.Interface;
 using Domain.Model;
+using Domain.Model.TokenAPIDto;
+using LoginAPI.Helpers;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net.WebSockets;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace LoginAPI.Controllers
 {
@@ -10,12 +20,13 @@ namespace LoginAPI.Controllers
     public class UserController : ControllerBase
     {
         private readonly IUserRepository _userRepo;
-
+        
         public UserController(IUserRepository userRepo)
         {
             _userRepo = userRepo;
         }
 
+        [Authorize]
         [HttpGet]
         public async Task<IActionResult> GetAllUser()
         {
@@ -30,6 +41,7 @@ namespace LoginAPI.Controllers
             }
         }
 
+        [Authorize]
         [HttpGet("id")]
         public async Task<IActionResult> GetOneUser(int id)
         {
@@ -47,18 +59,37 @@ namespace LoginAPI.Controllers
         [HttpPost]
         public async Task<IActionResult> InsertUser(UserModel user)
         {
+
             try
             {
+                if (user == null)
+                {
+                    return BadRequest();
+                }
+                var checkUsername = await _userRepo.CheckUsernameExist(user.Username);
+                //check same username
+                if (checkUsername)
+                {
+                    return BadRequest(new { Message = "Username Already Exists!" });
+                }
+                //check same email
+                if (await _userRepo.CheckEmailExist(user.EmailAddress))
+                {
+                    return BadRequest(new { Message = "Email Address Already Exists" });
+                }
                 var result = await _userRepo.InsertUser(user);
-                return Ok(result);
+                return Ok(new {Message = "User Added!"});
+
             }
             catch(Exception ex)
             {
                 return BadRequest(ex.Message);
             }
+
+            //user.Password = PasswordHash.HashPassword(user.Password);
         }
 
-        [HttpPut("id")]
+        [HttpPut]
         public async Task<IActionResult> UpdateUser(UserModel user)
         {
             try
@@ -72,7 +103,7 @@ namespace LoginAPI.Controllers
             }
         }
 
-        [HttpDelete("id")]
+        [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteUser(int id)
         {
             try
@@ -85,5 +116,56 @@ namespace LoginAPI.Controllers
                 return BadRequest(ex.Message);
             }
         }
+
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate(UserModel user)
+        {
+        
+
+            if (user == null)
+            {
+                return BadRequest();
+            }
+            var result = await _userRepo.AuthenticateLogin(user);
+            if (result == null)
+            {
+                return NotFound(new { Message = "User not found" });
+            }
+            result.Token = CreateJWTToken(result);
+
+            return Ok(new
+            {
+                Token = result.Token,
+                Message = "Login Successful"
+            });
+        }
+
+        private string CreateJWTToken(UserModel user)
+        {
+            var jwtTokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("secret45353454354jdfg");
+            var identity = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.Email, user.EmailAddress),
+                new Claim(ClaimTypes.Role, user.Role),
+            });
+
+            var creditials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = identity,
+                Expires = DateTime.Now.AddSeconds(120),
+                SigningCredentials = creditials,
+            };
+
+            var token = jwtTokenHandler.CreateToken(tokenDescriptor);
+            return jwtTokenHandler.WriteToken(token);
+        }
+
+    
+
+
     }
 }
--- API/LoginAPI/Program.cs
+++ API/LoginAPI/Program.cs
@@ -1,6 +1,10 @@
 using DapperContext;
 using DataConnections;
 using Domain.Interface;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,10 +13,37 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<DataContext>();
 builder.Services.AddScoped<IUserRepository, UserConnections>();
 
+builder.Services.AddCors(opt => opt.AddPolicy(name: "login",
+    policy =>
+    {
+        policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader().AllowCredentials();
+    }));
+
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("secret45353454354jdfg")),
+        ValidateAudience = false,
+        ValidateIssuer = false,
+        ClockSkew = TimeSpan.Zero
+  
+    };
+});
+
 
 var app = builder.Build();
 
@@ -23,7 +54,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("login");
+
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
