a41d5598c52e8479e6ddda80f51fe8e161e6b0df
RealAbu0 <105455070+RealAbu0@users.noreply.github.com>
2023-04-23 19:09:30 +0100

    Add files via upload

--- API/Domain/Interface/IUserRepository.cs
+++ API/Domain/Interface/IUserRepository.cs
@@ -9,10 +9,17 @@ namespace Domain.Interface
 {
     public interface IUserRepository
     {
+        
         Task<IEnumerable<UserModel>> GetAllUsers();
         Task<UserModel> GetOneUserByID(int id);
         Task<UserModel> InsertUser(UserModel user);
         Task<UserModel> UpdateUser(UserModel user);
         Task<UserModel> DeleteUser(int id);
+        Task<UserModel> AuthenticateLogin(UserModel user);
+        Task<bool> CheckUsernameExist(string user);
+        Task<bool> CheckEmailExist(string user);
+        string GetRefreshToken();
+
+
     }
 }
--- API/Domain/Model/UserModel.cs
+++ API/Domain/Model/UserModel.cs
@@ -13,5 +13,10 @@ namespace Domain.Model
         public int PhoneNumber { get; set; }
         public string EmailAddress { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
+        public string Token { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+
+        public string RefreshToken { get; set; } = string.Empty;
+        public DateTime RefreshTokenExpiryTime { get; set; }
     }
 }
